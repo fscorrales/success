5e2637ee496496b0327c479827d8b9a335288a97
Martin-Juncos <prof.mcjuncos@gmail.com>
2024-11-26 16:11:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  const success = document.getElementById("success");
+  // Puedes agregar alguna lógica en JS si necesitas animación adicional o efectos.
+  console.log("Page loaded, success running...");
+};
